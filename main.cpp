dbfea573e09a8d70791dc87d29a2ac809654ebf7
roy4801 <a82611141@gmail.com>
2022-03-19 12:05:42 +0800

    Add glfw, glad, and spdlog and add first glfw window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "parser.h"
 
-int main() {
-	objParser *op = new objParser("./bunny.obj");
+int main(){
+//	objParser *op = new objParser("./bunny.obj");
+//	op -> printVertexes();
 
-	op -> printVertexes();
+    glfwInit();
+    // Specify OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	return 0;
+    GLFWwindow *window = glfwCreateWindow(800, 600, "krkr", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << '\n';
+        glfwTerminate();
+        return 1;
+    }
+    // Load OpenGL Context
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 600);
+    glClearColor(.75f, .49f, .28f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }

fd278e928e205a8d929ff6eb99c85ba78ab37e5b
kirakira-pp <kirakira-pp.cs09g@nctu.edu.tw>
2022-03-19 14:44:46 +0800

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 #include "parser.h"
+#include "Log.h"
+
+struct Test
+{
+    int a;
+
+    friend std::ostream& operator<<(std::ostream& out, const Test &test)
+    {
+        out << test.a;
+        return out;
+    }
+};
+
+template <>
+struct fmt::formatter<Test>: formatter<std::string> {
+    template <typename FormatContext>
+    auto format(Test t, FormatContext& ctx) {
+        auto ans = fmt::format("Fuck = {}", t.a);
+        return formatter<std::string>::format(ans, ctx);
+    }
+};
 
 int main(int argc, char* argv[]) {
 	objParser *op = new objParser(argv[1]);
@@ -11,12 +35,24 @@ int main(int argc, char* argv[]) {
 	op -> printVertexes();
 #endif
 
+    INFO("test");
+    TRACE("123123");
+    Test t;
+    DEBUG("{} {} {} {} {}", 1, "2", 3.f, 4.0, t);
+
     glfwInit();
     // Specify OpenGL version
+#if __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+    
     GLFWwindow *window = glfwCreateWindow(800, 600, "krkr", nullptr, nullptr);
     if(window == nullptr)
     {

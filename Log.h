8e86fbc4084cf91daa882898947ce24ee1b3bc70
roy4801 <a82611141@gmail.com>
2022-03-19 14:18:24 +0800

    Add libraries fmt and glm and implement logger

---
+++ Log.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef KR2_OBJ_PARSER_LOG_H
+#define KR2_OBJ_PARSER_LOG_H
+
+#include <string>
+#include <memory>
+#include <fmt/format.h>
+
+#define TRACE(...) g_Logger.LogMessage(Log::LogLevelTrace, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define DEBUG(...) g_Logger.LogMessage(Log::LogLevelDebug, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define INFO(...) g_Logger.LogMessage(Log::LogLevelInfo, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define WARN(...) g_Logger.LogMessage(Log::LogLevelWarn, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define ERROR(...) g_Logger.LogMessage(Log::LogLevelError, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define CRITICAL(...) g_Logger.LogMessage(Log::LogLevelCritical, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+
+class Log
+{
+public:
+    Log();
+    ~Log();
+
+    enum LogLevel
+    {
+        LogLevelTrace = 0,
+        LogLevelDebug,
+        LogLevelInfo,
+        LogLevelWarn,
+        LogLevelError,
+        LogLevelCritical,
+        LogLevelCount
+    };
+
+    template<typename ... Args>
+    void LogMessage(LogLevel level, const char* file, int line, const char* function, const std::string &fmt, Args && ... args)
+    {
+        auto formatted = fmt::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...));
+        LogMessageImpl(level, file, line, function, formatted);
+    }
+
+    void LogMessage(LogLevel level, const char* file, int line, const char* function, const std::string &s)
+    {
+        LogMessageImpl(level, file, line, function, s);
+    }
+private:
+    void LogMessageImpl(LogLevel level, const char* file, int line, const char* function, const std::string &s);
+
+    struct Impl;
+    friend class Impl;
+    std::unique_ptr<Impl> m_Impl;
+};
+
+extern Log g_Logger;
+
+#endif //KR2_OBJ_PARSER_LOG_H
--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 #include "parser.h"
+#include "Log.h"
+
+struct Test
+{
+    int a;
+
+    friend std::ostream& operator<<(std::ostream& out, const Test &test)
+    {
+        out << test.a;
+        return out;
+    }
+};
+
+template <>
+struct fmt::formatter<Test>: formatter<std::string> {
+    template <typename FormatContext>
+    auto format(Test t, FormatContext& ctx) {
+        auto ans = fmt::format("Fuck = {}", t.a);
+        return formatter<std::string>::format(ans, ctx);
+    }
+};
 
 int main(int argc, char* argv[]) {
 	objParser *op = new objParser(argv[1]);
@@ -10,6 +34,11 @@ int main(int argc, char* argv[]) {
 #ifdef VERBOSE
 	op -> printVertexes();
 #endif
+
+    INFO("test");
+    TRACE("123123");
+    Test t;
+    DEBUG("{} {} {} {} {}", 1, "2", 3.f, 4.0, t);
 
     glfwInit();
     // Specify OpenGL version
